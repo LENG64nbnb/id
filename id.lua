81ce1e19d29df92e089095be575e785a9a321983
LENG64nbnb <264569636@qq.com>
2024-12-29 13:25:33 +0800

    Add files via upload

---
+++ id.lua
@@ -0,0 +1,34 @@
+if game.PlaceId == 6839171747 then--doors
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id/refs/heads/main/Doors.lua"))()
+elseif game.PlaceId == 3623096087 then--力量传奇
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id/refs/heads/main/%E5%8A%9B%E9%87%8F%E4%BC%A0%E5%A5%87.lua"))()
+elseif game.PlaceId == 7239319209 then--Ohio
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id/refs/heads/main/%E4%BF%84%E4%BA%A5%E4%BF%84%E5%B7%9E.lua"))()
+elseif game.PlaceId == 4618049391 then--河北唐县
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id2/refs/heads/main/%E6%B2%B3%E5%8C%97.lua"))()
+elseif game.PlaceId == 18241512421 then-- 狗熊岭危机
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id2/refs/heads/main/%E7%8B%97%E7%86%8A%E5%B2%AD.lua"))()
+elseif game.PlaceId == 5256165620 then-- 火箭发射模拟器
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id2/refs/heads/main/%E7%81%AB%E7%AE%AD%E5%8F%91%E5%B0%84%E6%A8%A1%E6%8B%9F%E5%99%A8.lua"))()
+elseif game.PlaceId == 12931609417 then-- 颜色或死亡
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id/refs/heads/main/%E5%86%B7%E8%84%9A%E6%9C%AC%E9%A2%9C%E8%89%B2%E6%88%96%E8%80%85%E6%AD%BB%E4%BA%A1%E6%9C%8D%E5%8A%A1%E5%99%A8%E6%BA%90%E7%A0%81.lua"))()
+elseif game.PlaceId == 110333320616502 then-- 格蕾丝
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id2/refs/heads/main/%E5%86%B7%E8%84%9A%E6%9C%AC%E6%A0%BC%E8%95%BE%E4%B8%9D%E6%BA%90%E7%A0%81.lua"))()
+elseif game.PlaceId == 138837502355157 then-- 格蕾丝
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id2/refs/heads/main/%E5%86%B7%E8%84%9A%E6%9C%AC%E6%A0%BC%E8%95%BE%E4%B8%9D%E6%BA%90%E7%A0%81.lua"))()
+elseif game.PlaceId == 13822889 then-- 伐木大亨2
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/id/refs/heads/main/%E4%BC%90%E6%9C%A8%E5%A4%A7%E4%BA%A8.lua"))()
+else--通用
+    LENGScript = "冷脚本"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG64nbnb/leng-/refs/heads/main/%E5%86%B7%E8%84%9A%E6%9C%AC%E6%96%B0%E6%BA%90%E7%A0%81.lua"))()
+end
